97147b3aecd169e485d80ad0f3ab652c780a49f2
Alexey <alexey@Alexeys-MBP.lan>
2023-06-18 12:24:59 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,42 @@
 'use client'
-import React, { useState } from 'react';
-import { TextField, Button, Container, Typography } from '@mui/material';
+import React, { useState, useRef } from 'react';
+import { TextField, Container, Typography } from '@mui/material';
+import { v4 as uuid } from 'uuid';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const formRef = useRef();
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
+    fetch('https://api.mesto.co/v1/auth/getRefreshTokenByPassword', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password, RqUid:uuid() }),
+        }).then((res) => {
 
-    // Insert your logic for authentication here
-    console.log(`Username: ${username}, Password: ${password}`);
-  };
+            if (res.ok) {
+                res.json().then((data) => {
+                    console.log(data);
+                })
+                // return res.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+      console.log(`Username: ${email}, Password: ${password}`);
+    }
 
   return (
-      <Container>
+      <Container className='w-[500px]'>
         <Typography variant="h4">Login</Typography>
         <form onSubmit={handleSubmit}>
           <TextField
               label="Username"
               variant="outlined"
               margin="normal"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
               fullWidth
           />
           <TextField
@@ -34,14 +48,10 @@ const LoginForm = () => {
               onChange={e => setPassword(e.target.value)}
               fullWidth
           />
-          <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              fullWidth
-          >
-            Submit
-          </Button>
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full mt-4 rounded">
+                Submit
+            </button>
         </form>
       </Container>
   );
